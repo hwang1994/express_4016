897e576e6e1420d9fc013d7b660afd779fdd2f61
Henry Wang <hwang94@hotmail.com>
2023-10-28 15:22:03 -0400

    busywait and persistent storage

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const fs = require('fs')
 
 var app = express();
 
@@ -33,6 +34,34 @@ app.get('/secretValue', (req, res) => {
 });
 app.get('/envValue', (req, res) => {
     res.send(process.env.envValue);
+});
+
+app.post('/saveString', (req, res) => {
+    fs.writeFile('assignment3/string.txt', req.body.data, (err) => {          
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send('saved ' + req.body.data);
+        }
+    });
+});
+app.get('/getString', (req, res) => {
+    fs.readFile('assignment3/string.txt', (err, data) => {
+        if (err) {
+            res.sendStatus(404);
+        }
+        else {
+            res.send(data.toString());
+        }
+    })
+});
+app.get('/busywait', (req, res) => {
+	let result = 0;	
+	for (var i = Math.pow(parseInt(process.env.envValue), 7); i >= 0; i--) {		
+		result += Math.atan(i) * Math.tan(i);
+	};
+    res.send('busywait result ' + result);
 });
 
 module.exports = app;
